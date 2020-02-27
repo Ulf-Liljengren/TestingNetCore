9e28ffe43ba6a0bae7a0b0ea3605aa45f746a7b5
Ulf.Liljengren <Ulf.Liljengren@volvo.com>
2020-02-27 11:08:43 +0100

    Added i.

--- NetCoreTesting/NetCoreTesting/Program.cs
+++ NetCoreTesting/NetCoreTesting/Program.cs
@@ -7,6 +7,8 @@ namespace NetCoreTesting
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int i = 1 + 1;
+            Console.WriteLine("i = " + i.ToString());
         }
     }
 }
