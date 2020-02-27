e92b881ea59a8d12186bd5fae11fadf3308e446b
Ulf.Liljengren <Ulf.Liljengren@volvo.com>
2020-02-27 10:41:12 +0100

    Project added.

---
+++ NetCoreTesting/NetCoreTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
